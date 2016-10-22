6514c4856e06330e1956b6ec9b9ee058fcce81cd
Mark Jeronimus <mark.jeronimus@gmail.com>
2016-10-23 00:00:16 +0200

    Added utilities

--- src/org/jnbt/NBTUtils.java
+++ src/org/jnbt/NBTUtils.java
@@ -37,12 +37,23 @@ package org.jnbt;
 
 //@formatter:on
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * A class which contains NBT-related utility methods.
  *
  * @author Graham Edgecombe
  */
 public final class NBTUtils {
+	public static final Pattern NBT_FILE_NAME_PATTERN = Pattern.compile("^(.*)\\.dat$");
+
 	private NBTUtils() {
 		throw new AssertionError("Not instantiable");
 	}
@@ -155,6 +166,25 @@ public final class NBTUtils {
 				return IntArrayTag.class;
 			default:
 				throw new IllegalArgumentException("[JNBT] Invalid tag type : " + type + '.');
+		}
+	}
+
+	public static List<CompoundTag> readNBTFiles(Collection<File> files, NBTCompression compression)
+			throws IOException {
+		List<CompoundTag> result = new ArrayList<>(files.size());
+
+		for (File file : files) {
+			CompoundTag tag = readNBTFile(file, compression);
+			result.add(tag);
+		}
+
+		return result;
+	}
+
+	public static CompoundTag readNBTFile(File levelFile, NBTCompression compression) throws IOException {
+		try (NBTInputStream in = new NBTInputStream(
+				new BufferedInputStream(new FileInputStream(levelFile)), compression)) {
+			return (CompoundTag)in.readTag();
 		}
 	}
 }
